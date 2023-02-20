fac860bdf97ba50f7ec0296e6c0ea491eb528898
Khoza Charles <khozacharles832@gmail.com>
2023-02-20 14:56:39 -0800

    fix bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
-}
+printf("Size of a char: %d", sizeof(a));
+printf("Size of int: %d", sizeof(b));
+printf("Size of long int: %d", sizeof(c));
+printf("Size of long long int: %d", sizeof(d));
+printf("Size of float: %d", sizeof(f));
 
+return (0);
+}
