1c31ebc0f564f5c1dcc170b168a060292ab19b58
Khoza Charles <khozacharles832@gmail.com>
2023-02-21 10:55:02 -0800

    print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isLower(int c);
+int _isAlpha(int n);
+int _abc(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
