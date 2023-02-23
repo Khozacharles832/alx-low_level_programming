ab1b230d40f819140a4e7970841a4cd1a335f13c
Khoza Charles <khozacharles832@gmail.com>
2023-02-22 23:26:10 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,7 @@
+main.h
+
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -9,3 +10,4 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
+
