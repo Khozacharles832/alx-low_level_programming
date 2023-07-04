1af3d27218d980f7b494dc83d4e1f38851e96838
Khoza Charles <khozacharles832@gmail.com>
2023-07-04 06:10:17 -0700

    sum all elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_nodeint - adds all the elements of a linked list
+ * @head: The pointer to the head
+ *
+ * Description: A function that adds all the elements of a
+ * linked list and returns the sum
+ *
+ * Return: the sum of all the data
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+
+	if (!head)
+		return (0);
+	while (head)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
