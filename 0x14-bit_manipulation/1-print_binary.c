cd477cf4c92d4128446a75d9743ee4ded17885ec
Khoza Charles <khozacharles832@gmail.com>
2023-07-06 13:20:38 -0700

    first commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a convrted number in binary
+ * @n: the number to convert
+ *
+ * Description: a function that converts its input to
+ * binary
+ *
+ * Return: the converted number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
