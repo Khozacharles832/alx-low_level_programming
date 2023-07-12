e6d4f390a8a63bf49ceb416e0df2ae5c918953e1
Khoza Charles <khozacharles832@gmail.com>
2023-07-12 04:02:58 -0700

    fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,21 +12,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fd;
-
-	fd = fopen(filename, "w");
-	if (fd == NULL)
-		return (-1);
+	int new_file, i;
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-		fputs(text_content, fd);
-
-	fclose(fd);
-
-	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
+	if (text_content == NULL)
+		text_content = "";
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (new_file == -1)
 		return (-1);
-	return (0);
+	write(new_file, text_content, i);
+
+	return (1);
 }
